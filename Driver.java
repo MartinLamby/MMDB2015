050d1127ca3dca2ac95127ea025e5b0b178d04d9
BenjaminGlanzer <benjaminglanzer@gmail.com>
2015-07-03 14:41:41 +0200

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,5 +1,37 @@
 package mmdb15;
 
 public class Driver {
-  //toDo
+  int avenue;
+  int street;
+  int distance;
+
+  public Driver (int avenue, int street, int distance){
+    this.avenue = avenue;
+    this.street = street;
+    this.distance = distance;
+  }
+
+public int getAvenue(){
+  return avenue;
+}
+
+public int getStreet(){
+  return street;
+}
+
+public int getDistance(){
+  return distance;
+}
+
+public void setDistance(int distance){
+   this.distance = distance;
+}
+
+public void setStreet(int street){
+   this.street = street;
+}
+
+public void setAvenue(int avenue){
+   this.avenue = avenue;
+}
 }
